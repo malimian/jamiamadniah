532448a6d337061948d0a657e50d63574eb1a94b
malimian <m.ali.mamoon@hotmail.com>
2025-03-15 16:04:19 +0500

    updates

--- db/db_connect.php
+++ db/db_connect.php
@@ -5,14 +5,14 @@ if($_SERVER['REMOTE_ADDR'] == "::1" || $_SERVER['REMOTE_ADDR'] == "127.0.0.1"){
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
-	$dbname = "travelntour";
+	$dbname = "ibspotlight";
 	$conn = new mysqli($servername, $username, $password, $dbname);
 }
 else{
 	$servername = "localhost";
-	$username = "travelntour";
-	$password = "travelntour";
-	$dbname = "travelntour";
+	$username = "ibspotlight9225";
+	$password = "ibspotlight9225";
+	$dbname = "ibspotlight9225";
 	$conn = new mysqli($servername, $username, $password, $dbname);	
 	//u
 }
