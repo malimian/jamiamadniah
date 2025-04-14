92f356c36a3dec31b67eb0aec02eb826f2147f18
malimian <m.ali.mamoon@hotmail.com>
2025-04-14 15:57:48 +0500

    updating front header , footer navbar

--- includes/footer.php
+++ includes/footer.php
@@ -1,28 +1,75 @@
-<?php 
-function front_footer($libs = null){
-        if(!empty($libs)){
-        foreach ($libs as $lib){
-            echo $lib;
-         }
+<?php
+/**
+ * Frontend Footer Function
+ * 
+ * @param array|null $libs Additional libraries/scripts to include before closing body
+ */
+function front_footer($libs = null) {
+    // Start output buffer
+    ob_start();
+    
+    // Output additional libraries if provided
+    if (!empty($libs)) {
+        echo "\n";
+        if (is_array($libs)) {
+            foreach ($libs as $lib) {
+                echo $lib . "\n";
+            }
+        } else {
+            echo $libs . "\n";
         }
+    }
+    
+    
+    // Flush output buffer
+    ob_end_flush();
 }
 
-
-function Basefooter($libs = null , $template_id = null){
+/**
+ * Base Footer Function with Template Support
+ * 
+ * @param array|null $libs Additional libraries/scripts to include
+ * @param int|null $template_id Template ID from database
+ * @return string Complete footer content
+ */
+function Basefooter($libs = null, $template_id = null) {
     global $and_gc;
-   
+    
+    // Start output buffer
+    ob_start();
+    
+    // Include loading modal
     require_once 'modals/loading.php';
-
-   $footer = "";
-
-   if(!empty($template_id)){
-
-	    $st_footer = return_single_ans("Select st_footer from site_template Where st_id = $template_id $and_gc and isactive = 1 ");
-
-	        $footer = $st_footer;
-	   }
-
-	    return $libs."
-
-      ".$footer;
+    
+    // Get template footer if template_id is provided
+    $footer = '';
+    if (!empty($template_id)) {
+        $template_id = filter_var($template_id, FILTER_VALIDATE_INT);
+        if ($template_id !== false) {
+            $st_footer = return_single_ans(
+                "SELECT st_footer FROM site_template WHERE st_id = $template_id $and_gc AND isactive = 1" 
+            );
+            $footer = $st_footer ?? '';
+        }
+    }
+    
+    // Output additional libraries if provided
+    if (!empty($libs)) {
+        echo "\n";
+        if (is_array($libs)) {
+            foreach ($libs as $lib) {
+                echo $lib . "\n";
+            }
+        } else {
+            echo $libs . "\n";
+        }
+    }
+    
+    // Output template footer
+    echo $footer;
+    
+    // Get and clean buffer
+    $output = ob_get_clean();
+    
+    return $output;
 }
--- includes/header.php
+++ includes/header.php
@@ -1,110 +1,152 @@
 <?php
-function front_header($title = null ,  $keywords = null  ,$description = null ,$libs = null){
-?><!DOCTYPE html>
-<html>
-<head>
-    <title><?php echo $title;?></title>
-    <meta name="title" content="<?php echo $title;?>">
-    <meta name="description" content="<?php echo $description;?>">
-    <meta name="keywords" content="<?php echo $keywords;?>">
-    <meta name="robots" content="index, follow">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="language" content="English">
-    <meta name="author" content="<?php Company;?>">
-    <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
-    <script src="js_settings.js"></script>
-    <?php
-    $lang = "en";
-    if(isset($_GET['lang'])){
-        $lang = $_GET['lang'];
-        setcookie('googtrans', "/en/".$lang);
+/**
+ * Frontend Header Function
+ * 
+ * @param string|null $title Page title
+ * @param string|null $keywords Meta keywords
+ * @param string|null $description Meta description
+ * @param array|null $libs Additional libraries/scripts
+ */
+function front_header($title = null, $keywords = null, $description = null, $libs = null) {
+    // Sanitize inputs
+    $title = htmlspecialchars($title ?? '', ENT_QUOTES, 'UTF-8');
+    $keywords = htmlspecialchars($keywords ?? '', ENT_QUOTES, 'UTF-8');
+    $description = htmlspecialchars($description ?? '', ENT_QUOTES, 'UTF-8');
+    
+    // Determine language
+    $lang = 'en';
+    if (isset($_GET['lang'])) {
+        $lang = preg_replace('/[^a-z]/', '', strtolower($_GET['lang']));
+        setcookie('googtrans', "/en/{$lang}", 0, '/', '', false, true);
     }
     
+    // Start output
+    ob_start();
     ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
-            <script type="text/javascript">
-            function googleTranslateElementInit() {
-              new google.translate.TranslateElement({pageLanguage: 'en' , includedLanguages : 'ar,ur,en' , layout: google.translate.TranslateElement.InlineLayout.SIMPLE }, 'google_translate_element');
-            }
-            </script>
-            
-            <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-
+    <title><?= $title ?></title>
+    <meta name="title" content="<?= $title ?>">
+    <meta name="description" content="<?= $description ?>">
+    <meta name="keywords" content="<?= $keywords ?>">
+    <meta name="robots" content="index, follow">
+    <meta name="language" content="English">
+    <meta name="author" content="<?= htmlspecialchars(COMPANY_NAME ?? 'Company', ENT_QUOTES, 'UTF-8') ?>">
+    
+    <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+    
+    <!-- Google Translate -->
+    <script type="text/javascript">
+    function googleTranslateElementInit() {
+        new google.translate.TranslateElement({
+            pageLanguage: 'en',
+            includedLanguages: 'ar,ur,en',
+            layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+        }, 'google_translate_element');
+    }
+    </script>
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    
     <?php
-
-        if(!empty($libs)){
-        foreach ($libs as $lib){
-            echo $lib;
-         }
+    // Output additional libraries
+    if (!empty($libs) && is_array($libs)) {
+        foreach ($libs as $lib) {
+            echo $lib . "\n";
         }
-     ?>
-
-
+    }
+    ?>
 </head>
-
-
-<body>
-
-<?php }?>
-
-
 <?php
-function Baseheader($title = null ,  $keywords = null  ,$description = null ,$libs = null , $template_id = null){
-    
+    echo ob_get_clean();
+}
+
+/**
+ * Base Header Function with Template Support
+ * 
+ * @param string|null $title Page title
+ * @param string|null $keywords Meta keywords
+ * @param string|null $description Meta description
+ * @param array|null $libs Additional libraries/scripts
+ * @param int|null $template_id Template ID from database
+ */
+function Baseheader($title = null, $keywords = null, $description = null, $libs = null, $template_id = null) {
     global $and_gc;
-   
-   $header = "";
-
-   if(!empty($template_id)){
-
-    $site_header = return_single_ans("Select st_header from site_template Where st_id = $template_id $and_gc and isactive = 1 ");
-
-        $header = $site_header;
-   }
-
-?><!DOCTYPE html>
-<html>
-<head>
-    <title><?php echo $title;?></title>
-    <meta name="title" content="<?php echo $title;?>">
-    <meta name="description" content="<?php echo $description;?>">
-    <meta name="keywords" content="<?php echo $keywords;?>">
-    <meta name="robots" content="index, follow">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="language" content="English">
-    <meta name="author" content="<?php Company;?>">
-    <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
     
-    <?php
-    $lang = "en";
-    if(isset($_GET['lang'])){
-        $lang = $_GET['lang'];
-        setcookie('googtrans', "/en/".$lang);
+    // Sanitize inputs
+    $title = htmlspecialchars($title ?? '', ENT_QUOTES, 'UTF-8');
+    $keywords = htmlspecialchars($keywords ?? '', ENT_QUOTES, 'UTF-8');
+    $description = htmlspecialchars($description ?? '', ENT_QUOTES, 'UTF-8');
+    $template_id = filter_var($template_id, FILTER_VALIDATE_INT);
+    
+    // Get template header if template_id is provided
+    $header = '';
+    if (!empty($template_id)) {
+        $site_header = return_single_ans("SELECT st_header FROM site_template WHERE st_id = $template_id $and_gc AND isactive = 1");
+        $header = $site_header ?? '';
     }
     
-    ?>
+    // Determine language
+    $lang = 'en';
+    if (isset($_GET['lang'])) {
+        $lang = preg_replace('/[^a-z]/', '', strtolower($_GET['lang']));
+        setcookie('googtrans', "/en/{$lang}", 0, '/', '', false, true);
+    }
     
-            <script type="text/javascript">
-            function googleTranslateElementInit() {
-              new google.translate.TranslateElement({pageLanguage: 'en' , includedLanguages : 'ar,ur,en' , layout: google.translate.TranslateElement.InlineLayout.SIMPLE }, 'google_translate_element');
-            }
-            </script>
-            
-            <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-
-            <script type="text/javascript">
-                 var islive_streaming = <?php echo return_single_ans("Select isactive from category Where catid = 124 ");?>;
-           </script>
-
-            
+    // Start output
+    ob_start();
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <title><?= $title ?></title>
+    <meta name="title" content="<?= $title ?>">
+    <meta name="description" content="<?= $description ?>">
+    <meta name="keywords" content="<?= $keywords ?>">
+    <meta name="robots" content="index, follow">
+    <meta name="language" content="English">
+    <meta name="author" content="<?= htmlspecialchars(COMPANY_NAME ?? 'Company', ENT_QUOTES, 'UTF-8') ?>">
+    
+    <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+    
+    <!-- Google Translate -->
+    <script type="text/javascript">
+    function googleTranslateElementInit() {
+        new google.translate.TranslateElement({
+            pageLanguage: 'en',
+            includedLanguages: 'ar,ur,en',
+            layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+        }, 'google_translate_element');
+    }
+    
+    // Live streaming status
+    var islive_streaming = <?= json_encode(return_single_ans("SELECT isactive FROM category WHERE catid = 124")) ?>;
+    </script>
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    
+    <?= $header ?>
     <?php
-      echo $header;
-      echo $libs;
+    // Output additional libraries
+    if (!empty($libs)) {
+        if (is_array($libs)) {
+            foreach ($libs as $lib) {
+                echo $lib . "\n";
+            }
+        } else {
+            echo $libs . "\n";
+        }
+    }
     ?>
-
 </head>
-
-
-<body>
-    
-<?php }?>
+<?php
+    echo ob_get_clean();
+}
+?>
