fcb3ed82685a1ad457b84e75d4bbe700f9e6d9d1
malimian <m.ali.mamoon@hotmail.com>
2025-04-14 16:04:53 +0500

    Update scripts.php

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,33 +1,46 @@
-<?php 
-function front_script($libs = null){
-        if(!empty($libs)){
-        foreach ($libs as $lib){
-            echo $lib;
-         }
+<?php
+
+/**
+ * Echoes a list of front-end scripts.
+ *
+ * @param array|null $libs Array of script tags or URLs to include.
+ */
+function front_script($libs = null) {
+    if (!empty($libs) && is_array($libs)) {
+        foreach ($libs as $lib) {
+            echo $lib . "\n"; // Added newline for better HTML output formatting
         }
- }
-
-
-function BaseScript($libs = null , $template_id = null){
-    global $and_gc;
-   
-   $scripts = "";
-
-   if(!empty($template_id)){
-
-        $st_scripts = return_single_ans("Select st_script from site_template Where st_id = $template_id $and_gc and isactive = 1 ");
-
-            $scripts = $st_scripts;
-       }
-
-        return "
-
-      ".$scripts."
-
-      ".$libs."
-
-      ";
+    }
 }
 
+/**
+ * Returns scripts for the selected base template and custom libs.
+ *
+ * @param string|null $libs HTML string containing script tags or links.
+ * @param int|null $template_id Template ID from the database.
+ * @return string Combined HTML string of scripts.
+ */
+function BaseScript($libs = null, $template_id = null) {
+    global $and_gc;
+
+    $scripts = '';
+
+    if (!empty($template_id)) {
+        // Make sure to escape your variables properly to avoid SQL injection
+        $template_id = (int)$template_id;
+
+        $query = "SELECT st_script FROM site_template WHERE st_id = $template_id $and_gc AND isactive = 1";
+        $st_scripts = return_single_ans($query);
+
+        if (!empty($st_scripts)) {
+            $scripts = $st_scripts;
+        }
+    }
+
+    return <<<HTML
+{$scripts}
+{$libs}
+HTML;
+}
 
 ?>
