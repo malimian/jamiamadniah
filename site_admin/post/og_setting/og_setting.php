aeed35b209c88901be89421461dade15c7349269
malimian <m.ali.mamoon@hotmail.com>
2025-04-25 15:46:11 +0500

    Update og_setting.php

--- site_admin/post/og_setting/og_setting.php
+++ site_admin/post/og_setting/og_setting.php
@@ -3,21 +3,20 @@ require_once('../../admin_connect.php');
 
 if(isset($_POST['submit'])){
 
-$title = escape($_POST['title']);    
-$tagline =escape($_POST['tagline']);
-$url =$_POST['url'];
-$email =$_POST['email'];
-$key =$_POST['key'];
-$key_pass =$_POST['key_pass'];
-$env =$_POST['env'];
-$logo =$_POST['logo'];
-$img_path =$_POST['img_path'];
-$time_zone =$_POST['time_zone'];
-$file_path =$_POST['file_path'];
-$uid = $_SESSION['user']['id'];
-$friendly_url = $_POST['friendly_url'];
-$page_loader = $_POST['page_loader'];
-
+    $title = escape($_POST['title']);    
+    $tagline =escape($_POST['tagline']);
+    $url =$_POST['url'];
+    $email =$_POST['email'];
+    $key =$_POST['key'];
+    $key_pass =$_POST['key_pass'];
+    $env =$_POST['env'];
+    $logo =$_POST['logo'];
+    $img_path =$_POST['img_path'];
+    $time_zone =$_POST['time_zone'];
+    $file_path =$_POST['file_path'];
+    $uid = $_SESSION['user']['id'];
+    $friendly_url = $_POST['friendly_url'];
+    $page_loader = $_POST['page_loader'];
 
     // Update main settings in the database
     $updates = [
@@ -44,35 +43,24 @@ $page_loader = $_POST['page_loader'];
         update("UPDATE og_settings SET settings_value = '$value' WHERE settings_id = $setting_id");
     }
 
+    // Social media platforms with their corresponding IDs
+    $social_media_platforms = [
+        19 => isset($_POST['social_media_data']['facebook_url']) ? $_POST['social_media_data']['facebook_url'] : '',
+        20 => isset($_POST['social_media_data']['twitter_url']) ? $_POST['social_media_data']['twitter_url'] : '',
+        21 => isset($_POST['social_media_data']['instagram_url']) ? $_POST['social_media_data']['instagram_url'] : '',
+        22 => isset($_POST['social_media_data']['linkedin_url']) ? $_POST['social_media_data']['linkedin_url'] : '',
+        23 => isset($_POST['social_media_data']['youtube_url']) ? $_POST['social_media_data']['youtube_url'] : '',
+        24 => isset($_POST['social_media_data']['pinterest_url']) ? $_POST['social_media_data']['pinterest_url'] : '',
+        25 => isset($_POST['social_media_data']['snapchat_url']) ? $_POST['social_media_data']['snapchat_url'] : ''
+    ];
 
-
-$social_media_platforms = [
-    'facebook' => isset($_POST['social_media_data']['facebook_url']) ? $_POST['social_media_data']['facebook_url'] : '',
-    'twitter' => isset($_POST['social_media_data']['twitter_url']) ? $_POST['social_media_data']['twitter_url'] : '',
-    'instagram' => isset($_POST['social_media_data']['instagram_url']) ? $_POST['social_media_data']['instagram_url'] : '',
-    'linkedin' => isset($_POST['social_media_data']['linkedin_url']) ? $_POST['social_media_data']['linkedin_url'] : '',
-    'youtube' => isset($_POST['social_media_data']['youtube_url']) ? $_POST['social_media_data']['youtube_url'] : '',
-    'pinterest' => isset($_POST['social_media_data']['pinterest_url']) ? $_POST['social_media_data']['pinterest_url'] : '',
-    'snapchat' => isset($_POST['social_media_data']['snapchat_url']) ? $_POST['social_media_data']['snapchat_url'] : '',
-    'tiktok' => isset($_POST['social_media_data']['tiktok_url']) ? $_POST['social_media_data']['tiktok_url'] : '',
-    'reddit' => isset($_POST['social_media_data']['reddit_url']) ? $_POST['social_media_data']['reddit_url'] : '',
-    'whatsapp' => isset($_POST['social_media_data']['whatsapp_url']) ? $_POST['social_media_data']['whatsapp_url'] : '',
-    'telegram' => isset($_POST['social_media_data']['telegram_url']) ? $_POST['social_media_data']['telegram_url'] : ''
-];
-
-
-    // Loop through each social media platform and update the corresponding URL
-    foreach ($social_media_platforms as $platform => $url) {
-           	
-           	$platform = strtoupper($platform);
-
-            Update("UPDATE og_settings SET settings_value = '$url' WHERE settings_id = (SELECT settings_id FROM og_settings WHERE settings_name = '$platform' LIMIT 1)");
+    // Loop through each social media platform and update the corresponding URL by ID
+    foreach ($social_media_platforms as $platform_id => $url) {
+        if (!empty($url)) {
+            Update("UPDATE og_settings SET settings_value = '$url' WHERE settings_id = $platform_id");
+        }
     }
 
-
-
-echo "1";
-
+    echo "1";
 }
-
 ?>
