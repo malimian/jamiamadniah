74cd88ed1ce146091024424c752fff75ca895a19
malimian <m.ali.mamoon@hotmail.com>
2025-05-02 20:10:14 +0500

    updates

--- site_admin/editpage.php
+++ site_admin/editpage.php
@@ -254,41 +254,44 @@ AdminHeader(
                         <div class="ib-action-bar card">
                             <div class="card-body py-2">
                                 <div class="d-flex align-items-center">
+                                    <?php if($has_add): ?>
+                                        <a href="#" class="btn btn-sm btn-danger mr-2" data-toggle="modal" data-target="#addPageModal">
+                                            <i class="fa fa-plus fa-fw"></i> Add New Page
+                                        </a>
+                                    <?php endif; ?>
 
-                                            <?php if($has_add && canEdit($page['isSystemOperated'])): ?>
-                                            <a class="btn btn-sm btn-outline-secondary mr-2 duplicate-page" href="#" data-pageid="<?=$page['pid']?>">
-                                                <i class="fas fa-copy fa-fw"></i> Duplicate this page
-                                            </a>
-                                            <?php endif; ?>
+                                    <?php if($has_add && canEdit($page['isSystemOperated'])): ?>
+                                        <a class="btn btn-sm btn-outline-secondary mr-2 duplicate-page" href="#" data-pageid="<?=$page['pid']?>">
+                                            <i class="fas fa-copy fa-fw"></i> Duplicate this page
+                                        </a>
+                                    <?php endif; ?>
 
-                                            <?php if($has_status && canActivate($page['isSystemOperated'])): ?>
-                                            <div class="dropdown d-inline-block mr-2">
-                                                <button class="btn btn-sm btn-outline-<?=$page['isactive'] ? 'success' : 'warning'?> dropdown-toggle"
-                                                        type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                    <i class="fas fa-power-off fa-fw"></i> <?=$page['isactive'] ? 'Active' : 'Inactive'?>
-                                                </button>
-                                                <div class="dropdown-menu">
-                                                    <a class="dropdown-item" href="#" onclick="toggleStatus(<?=$page['pid']?>, <?=$page['isactive']?>)">
-                                                        <i class="fas fa-power-off fa-fw"></i> <?=$page['isactive'] ? 'Deactivate' : 'Activate'?>
-                                                    </a>
-                                                </div>
+                                    <?php if($has_status && canActivate($page['isSystemOperated'])): ?>
+                                        <div class="dropdown d-inline-block mr-2">
+                                            <button class="btn btn-sm btn-outline-<?=$page['isactive'] ? 'success' : 'warning'?> dropdown-toggle"
+                                                    type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                <i class="fas fa-power-off fa-fw"></i> <?=$page['isactive'] ? 'Active' : 'Inactive'?>
+                                            </button>
+                                            <div class="dropdown-menu">
+                                                <a class="dropdown-item" href="#" onclick="toggleStatus(<?=$page['pid']?>, <?=$page['isactive']?>)">
+                                                    <i class="fas fa-power-off fa-fw"></i> <?=$page['isactive'] ? 'Deactivate' : 'Activate'?>
+                                                </a>
                                             </div>
-                                            <?php endif; ?>
-
-                                            <?php if($has_delete && canDelete($page['isSystemOperated'])): ?>
-                                            <button class="btn btn-sm btn-outline-danger" onclick="delete_(<?=$page['pid']?>)">
-                                                <i class="fas fa-trash fa-fw"></i> Move to Recycle Bin
-                                            </button>
-                                            <?php endif; ?>
-
-                                            <button class="btn btn-sm btn-outline-warning ml-2" onclick="if (confirm('Are you sure you want to discard all unsaved changes?')) { location.reload(); }">
-                                                <i class="fas fa-undo fa-fw"></i> Discard Changes
-                                            </button>
-
-                                            <div class="ml-auto">
-                                                </div>
                                         </div>
-                            </div>
+                                    <?php endif; ?>
+
+                                    <?php if($has_delete && canDelete($page['isSystemOperated'])): ?>
+                                        <button class="btn btn-sm btn-outline-danger" onclick="delete_(<?=$page['pid']?>)">
+                                            <i class="fas fa-trash fa-fw"></i> Move to Recycle Bin
+                                        </button>
+                                    <?php endif; ?>
+
+                                    <button class="btn btn-sm btn-outline-warning ml-2" onclick="if (confirm('Are you sure you want to discard all unsaved changes?')) { location.reload(); }">
+                                        <i class="fas fa-undo fa-fw"></i> Discard Changes
+                                    </button>
+
+                                    <div class="ml-auto"></div>
+                                </div>
                         </div>
                     </div>
                 </div>
