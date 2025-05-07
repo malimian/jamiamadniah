f3d75e73321ddee393eaf56c1f7980be77400450
malimian <m.ali.mamoon@hotmail.com>
2025-05-07 16:37:47 +0500

    update

---
+++ modules/module_gallery.php
@@ -0,0 +1,23 @@
+<?php
+$basePath = ABSOLUTE_IMAGEPATH;
+
+$images = array_diff(scandir($basePath), array('.', '..'));
+
+// Filter only image files (optional but recommended)
+$images = array_filter($images, function($file) use ($basePath) {
+    $ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+    return in_array($ext, ['jpg', 'jpeg', 'png', 'gif', 'webp']);
+});
+
+// Shuffle and get 9 random images
+shuffle($images);
+$images = array_slice($images, 0, 9);
+?>
+
+<?php foreach ($images as $img): ?>
+  <div class="col-4 mb-3">
+    <div class="rounded overflow-hidden">
+      <img src="<?= $basePath . $img ?>" class="img-zoomin img-fluid rounded w-100" alt="">
+    </div>
+  </div>
+<?php endforeach; ?>
