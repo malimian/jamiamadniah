b17ea61a9c39a1562f7b72f514e9b0bbfd22969d
malimian <m.ali.mamoon@hotmail.com>
2025-05-27 00:32:25 +0500

    Update module_comment.php

--- modules/module_comment.php
+++ modules/module_comment.php
@@ -1,34 +1,13 @@
-<div id="HCB_comment_box">Loading Comments...</div>
-
- <script type="text/javascript" id="hcb"> 
-
- 	/*<!--*/ 
-
- 	if (!window.hcb_user) {
- 	  hcb_user = {};
- 	}(function() {
- 	  var s = document.createElement("script"),
- 	    l = hcb_user.PAGE || ("" + window.location).replace(/'/g, "%27"),
- 	    h = "";
- 	 
- 	   s.setAttribute("type", "text/javascript");
- 	  
- 	  var kn = "https://www.htmlcommentbox.com/jread?page=" + encodeURIComponent(l).replace("+", "%2B") + "&mod=" + <?php echo COMMENT_API;?> + "&opts=16862&num=10&ts=1615119073229";
- 	  
- 	  console.log(kn);
-
- 	  s.setAttribute("src", kn);
-
- 	  if (typeof s != "undefined") document.getElementsByTagName("head")[0].appendChild(s);
-
- 	})();
-
-/*-->*/
-
- </script>
-
 <style>
 	.home-desc,#hcb_settings,.hcb-icon,#HCB_comment_box h3{
 		display: none;
 	}
 </style>
+
+
+<!-- begin wwww.htmlcommentbox.com -->
+
+ <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Comment Box</a> is loading comments...</div>
+ <link rel="stylesheet" type="text/css" href="https://www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
+ <script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="https://www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=<?php echo COMMENT_API;?>"+"&opts=16798&num=10&ts=1748287755604");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
+<!-- end www.htmlcommentbox.com -->
